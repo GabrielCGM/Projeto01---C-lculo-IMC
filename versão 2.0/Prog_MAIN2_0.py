ca050294315abfc69cb2f5eae0cdab251db2d765
GabrielCGM <107153200+GabrielCGM@users.noreply.github.com>
2022-07-13 21:37:49 -0300

    Delete Prog_MAIN2_0.py

--- versão 2.0/Prog_MAIN2_0.py
+++
@@ -1,8 +0,0 @@
-from calculo_imc import * #Importando as funções
-class Programa_MAIN(): #CLASSE DO PROGRAMA PRINCIPAL
-    def __init__(self) -> None:
-        import os
-        os.system('cls')
-        Funcionamento_PROG()
-
-Programa_MAIN()
