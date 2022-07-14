f28330976eaf7f8a742509cf3633141fad6cf7b6
GabrielCGM <107153200+GabrielCGM@users.noreply.github.com>
2022-07-13 21:01:27 -0300

    Add files via upload
    Melhorias no script deixando mais eficiente e limpo.

---
+++ versão 2.0/Prog_MAIN2_0.py
@@ -0,0 +1,8 @@
+from calculo_imc import * #Importando as funções
+class Programa_MAIN(): #CLASSE DO PROGRAMA PRINCIPAL
+    def __init__(self) -> None:
+        import os
+        os.system('cls')
+        Funcionamento_PROG()
+
+Programa_MAIN()
